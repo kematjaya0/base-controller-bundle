1ee89c100e2f86db7c8dc4584840744bf2245eab
Nur Hidayatullah <kematjaya0@gmail.com>
2021-01-23 11:29:11 +0700

    add class finder

---
+++ src/ClassFinder/KmjClassFinder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Kematjaya\BaseControllerBundle\ClassFinder;
+
+use HaydenPierce\ClassFinder\ClassFinder;
+use ReflectionClass;
+/**
+ * @author Nur Hidayatullah <kematjaya0@gmail.com>
+ */
+class KmjClassFinder 
+{
+    /**
+     * 
+     * @param string $namespace
+     * @param string $intefaceClass
+     * @return array
+     */
+    public static function getClassesInNamespaceByInterface(string $namespace, string $intefaceClass = null):array
+    {
+        $classes = ClassFinder::getClassesInNamespace($namespace);
+        
+        if (null === $intefaceClass) {
+            
+            return $classes;
+        }
+            
+        $data = [];
+        foreach ($classes as $class) {
+            $reflect = new ReflectionClass($class);
+            if (!$reflect->implementsInterface($intefaceClass)) {
+                continue;
+            }
+            
+            $data[$class] = $class;
+        }
+        
+        return $data;
+    }
+}
