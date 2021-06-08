d751b8ccd5d1c762c26833011f55270304945c7e
Nur Hidayatullah <kematjaya0@gmail.com>
2021-06-08 16:48:46 +0700

    add twig function

---
+++ src/Twig/ArrayExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the base-controller-bundle.
+ */
+
+namespace Kematjaya\BaseControllerBundle\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+/**
+ * @package Kematjaya\BaseControllerBundle\Twig
+ * @license https://opensource.org/licenses/MIT MIT
+ * @author  Nur Hidayatullah <kematjaya0@gmail.com>
+ */
+class ArrayExtension extends AbstractExtension 
+{
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('in_array', function($value, $arr) {
+                return in_array($value, $arr);
+            })
+        ];
+    }
+
+}
