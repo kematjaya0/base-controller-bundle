d10622992cbc95c0aa2c1b5d975ea08eb002788e
Programmer <kematjaya0@gmail.com>
2022-06-14 11:28:22 +0700

    entity auto complete

---
+++ src/Type/AutoCompleteEntityType.php
@@ -0,0 +1,114 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace Kematjaya\BaseControllerBundle\Type;
+
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Doctrine\Persistence\ManagerRegistry;
+use Kematjaya\HiddenTypeBundle\DataTransformer\ObjectToIdTransformer;
+use Kematjaya\HiddenTypeBundle\DataTransformer\Transformer;
+
+/**
+ * Description of AutoCompleteEntityType
+ *
+ * @author programmer
+ */
+class AutoCompleteEntityType extends AbstractType 
+{
+    
+    /**
+     * 
+     * @var ManagerRegistry
+     */
+    private $registry;
+    
+    /**
+     * 
+     * @var Transformer
+     */
+    private $transformer;
+    
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+    
+    public function getParent()
+    {
+        return TextType::class;
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {   
+        parent::configureOptions($resolver);
+        $resolver->setRequired(['url', "class", 'property_label']);
+        
+        $resolver->setDefaults([
+            'property'   => 'id'
+        ]);
+        
+        $resolver->setAllowedTypes('property', ['null', 'string']);
+        $resolver->setAllowedTypes('property_label', ['string']);
+    }
+    
+    /**
+    * {@inheritdoc}
+    */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->transformer = new ObjectToIdTransformer(
+            $this->registry,
+            $options['class'],
+            $options['property']
+        );
+        
+        $builder->addModelTransformer($this->transformer);
+    }
+    
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+        
+        $view->vars["attr"]["class"] = $view->vars["attr"]["class"] ?? "autocomplete form-control";
+        $view->vars["url"] = $options["url"];
+        $attr = $view->vars["attr"];
+        $view->vars["html_attributes"] = join(" ", array_map(function ($key) use ($attr) {
+            return sprintf('%s="%s"', $key, $attr[$key]);
+        }, array_keys($attr)));
+        
+        
+        
+        $view->vars["label_data"] = $this->getLabelData($view, $options);
+        
+    }
+    
+    protected function getLabelData(FormView $view, array $options):?string
+    {
+        if (null == $view->vars["data"]) {
+            
+            return null;
+        }
+        
+        $entity = $this->transformer->reverseTransform($view->vars["data"]);
+        if (null === $entity) {
+            return null;
+        }
+        
+        $accessor = PropertyAccess::createPropertyAccessor();
+        if (! $accessor->isReadable($entity, $options['property_label'])) {
+            return null;
+        }
+        
+        return $accessor->getValue($entity, $options['property_label']);
+    }
+}
