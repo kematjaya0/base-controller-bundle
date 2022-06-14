16026f33da9fd8d8793252525e38a11911a0fce7
Nur Hidayatullah <>
2022-06-14 08:05:11 +0700

    dev autocomplete type

--- src/Type/AutoCompleteType.php
+++ src/Type/AutoCompleteType.php
@@ -2,6 +2,8 @@
 
 namespace Kematjaya\BaseControllerBundle\Type;
 
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,12 +19,29 @@ class AutoCompleteType extends AbstractType
     
     public function configureOptions(OptionsResolver $resolver)
     {   
-        $resolver->setRequired('url');
+        parent::configureOptions($resolver);
+        $resolver->setRequired(['url', "dom_parent"]);
         $resolver->addNormalizer('attr', function (Options $options) {
             return [
-                'class' => 'form-control autocomplete', 'url' => $options['url']
+                'class' => 'autocomplete form-control', 'url' => $options['url']
             ];
         });
+        
+        
+        $resolver->setDefaults([
+            "dom_parent" => null
+        ]);
     }
     
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+        
+        $attr = $view->vars["attr"];
+        $view->vars["html_attributes"] = join(" ", array_map(function ($key) use ($attr) {
+            return sprintf('%s="%s"', $key, $attr[$key]);
+        }, array_keys($attr)));
+        
+        $view->vars["appendTo"] = $options["dom_parent"];
+    }
 }
