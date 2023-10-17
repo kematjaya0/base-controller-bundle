721b0a6bba1ffb26728c93fe107fd18c7dba4e18
Nur Hidayatullah <>
2023-10-18 05:52:18 +0700

    html5 options

--- src/Type/FloatRangeType.php
+++ src/Type/FloatRangeType.php
@@ -4,28 +4,38 @@ namespace Kematjaya\BaseControllerBundle\Type;
 
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FloatRangeType extends AbstractType
 {
-    
-    public function buildForm(FormBuilderInterface $builder, array $options) 
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $fromOpt = $options['from_options'];
         $fromOpt['required'] = false;
         $toOpt = $options['to_options'];
         $toOpt['required'] = false;
-        
-        $builder->add('from', NumberType::class, $fromOpt)
+
+        if (true === $options["html5"]) {
+            $fromOpt['html5'] = true;
+            $toOpt['html5'] = true;
+            $builder->add('from', NumberType::class, $fromOpt)
                 ->add('to', NumberType::class, $toOpt);
+            return;
+        }
+
+        $builder->add('from', TextType::class, $fromOpt)
+            ->add('to', TextType::class, $toOpt);
     }
-    
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'from_options' => [],
-            'to_options' => []
+            'to_options' => [],
+            "html5" => true
         ]);
     }
 }
