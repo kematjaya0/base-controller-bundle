13d461b0c9f3e63b6b2e402938ba88a0cb093515
Nur Hidayatullah <kematjaya0@gmail.com>
2026-01-18 12:20:22 +0700

    upgrade sf 8

--- src/BaseControllerBundle.php
+++ src/BaseControllerBundle.php
@@ -17,7 +17,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  */
 class BaseControllerBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container):void
     {
         $container->registerForAutoconfiguration(TwigControllerInterface::class)
             ->addTag("controller.twig_arguments");
